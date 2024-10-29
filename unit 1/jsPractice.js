c4bd5713cefea81d04e65ceec821e9f402150dab
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-10-29 17:40:02 +0000

    test

---
+++ unit 1/jsPractice.js
@@ -0,0 +1,13 @@
+//-for comment
+
+true,1
+false, 0
+
+
+var email= "Gear4snak3manluffy@gmail.com"
+var password= "Gear5luffy215"
+const dbPassword= "Gear5luffy215"
+
+let image1="";
+
+const text= "";
