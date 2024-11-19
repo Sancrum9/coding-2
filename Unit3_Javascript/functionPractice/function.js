6725516cfbd237a52016ea409dcca8a5a7ac9f5e
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-11-19 18:40:29 +0000

    done the work for tdy

---
+++ Unit3_Javascript/functionPractice/function.js
@@ -0,0 +1,12 @@
+name = "Sa'id Ancrum"
+console.log(name)
+// User created functions
+// declare then call
+function exampleFunction() {
+    alert("you are now connect to the js file")
+    console.log("step 1.");
+    console.log("step 2.");
+    console.log("step 3.");
+}
+
+exampleFunction()
