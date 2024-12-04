a8bff807b1cae7f46b4c878b505c2e34d1ae17c7
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-12-04 18:41:09 +0000

    done

---
+++ Unit3_Javascript/handsOnFunctions.js
@@ -0,0 +1,10 @@
+function numberTominutes(hour){
+    if (hour == 24){
+        console.log("This is a whole day.");
+    } else {
+    console.log (hour * 60 + "minutes");
+    console.log (hour * 120 + "minutes");
+    console.log (hour * 180 + "minutes");
+}
+}
+numberTominutes(60)
