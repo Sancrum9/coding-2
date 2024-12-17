afa25a7e709d535d89b168f0c9a5e503be85f0b7
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-12-17 18:40:36 +0000

    kinda finished

--- Unit3_Javascript/Aysnc_Activitydec13.js
+++ Unit3_Javascript/Aysnc_Activitydec13.js
@@ -1,16 +1,16 @@
 //1.Create a function that will determine if a number passed into it's funtion parameters is either positive or negative. For example, If I pass in the number 10
 // it should return the message,"this is a positive number", or if I type in the number -12, it should return the message, "this is a negative number.
 //  If the user types in zero, it should return the message; "this is zero".
-function numbers(number){
-    if (number > 0){
-        console.log( "this is a positive number");
-    } if (number < 0){
-        console.log( "This is a negative number");
-    } if (number = 0) {
-        console.log( "it is nor negative or positive");
-}
-}
-number()
+//function numbers(number){
+//    if (number > 0){
+//        console.log( "this is a positive number");
+//    } if (number < 0){
+//        console.log( "This is a negative number");
+//    } if (number == 0) {
+//        console.log( "it is nor negative or positive");
+//}
+//}
+//numbers(0);
 //2.You have been hired by netflix to help them develop a movie ticket program. You must create a function that will check the movie goers age and return
 //the price of the movie ticket based on that person's age. Provided are the lists of age and the prices:
 
@@ -18,6 +18,20 @@ number()
 //16 and up should pay $10.00
 //20 an up should pay $15.00
 //65 and up should pay 5.00
+
+function Prices(age){
+    if (age <= 10){
+        console.log( "Price will be 5$");
+    } if (age >= 16){
+        console.log( "Price will be 10$");
+    } if (age >= 20) {
+        console.log( "Price will be 15$");
+    }
+    if (age >= 65){
+        console.log( "Price will be 5$");
+}
+}
+Prices(100)
 
 //3.You have been hired by target to assist them with their store member discount software. The would like to make it so that shoppers who 
 // have a specific membership tier can save a certain amount of money on the products they buy. provided below are the memberships and the discount amount they should recieve:
