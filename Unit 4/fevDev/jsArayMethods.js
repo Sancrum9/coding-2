fa7a9fad9f9f409cbb44f1c9530d6768eaf2183c
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2025-01-16 18:42:40 +0000

    will finish tmr

---
+++ Unit 4/fevDev/jsArayMethods.js
@@ -0,0 +1,27 @@
+classes = ["math",
+    "reading",
+    "gym",
+    10,
+    10.23,
+    true,
+    "reading"];
+
+
+
+var bookBag=[ "pencils","pens","notebook","fiction book","homework"]
+bookBag.push("sketch book")
+console.log(bookBag)
+
+bookBag.pop()
+console.log(bookBag)
+
+console.log(bookBag.length)
+
+function amazonCart(item){
+    itemInCart.push(item);
+    console.log(itemInCart)
+}
+
+amazonCart(["pen","pencil","notebook"])
+
+itemsInCart
