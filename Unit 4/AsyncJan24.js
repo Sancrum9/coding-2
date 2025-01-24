0968093a7ad01506dd75db16b600469dae0721d5
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2025-01-24 20:11:27 +0000

    done

---
+++ Unit 4/AsyncJan24.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Loops</title>
+  </head>
+  <body>
+    <input type="text" id="userInput" />
+    <button onclick="runLoop()">Submit</button>
+<script>
+      function runLoop() {
+        var input = document.getElementById('0').value;
+        console.log(5,4,3,2,1,0 )
+        
+        
+      }
+</script> swrc=''
+  </body>
+</html>
